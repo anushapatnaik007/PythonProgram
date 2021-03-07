2e86e1d6b012e88aebbba68817a7f7146db757f5
ANUSHA PATNAIK <77797806+anushapatnaik007@users.noreply.github.com>
2021-03-07 11:27:51 +0530

    Uploaded code for printing table for any number

---
+++ Any_table.py
@@ -0,0 +1,7 @@
+number = int(input("Enter the multiplication table you want to enter: "))
+print("Multiplication table of",table,"is :")
+
+for i in range(1,11):
+    print(number, "X", i, "=", number*i)
+
+
