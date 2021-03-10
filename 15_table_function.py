8390fb9619dd555a412bf68e7c8a7dd190db6180
ANUSHA PATNAIK <77797806+anushapatnaik007@users.noreply.github.com>
2021-03-11 01:11:36 +0530

    function to multiply 15 Table

---
+++ 15_table_function.py
@@ -0,0 +1,11 @@
+def print_mtable():
+    """ 
+        Describe what this function does
+    """
+    # Your solution code goes in here
+    num = 15
+    for i in range(1,11):
+        print(num, "x", i, "=", num*i)
+
+# Main starts from here
+print_mtable()
